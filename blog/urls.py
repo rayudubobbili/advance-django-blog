82369fab27a2b60057f71e821431f4b75010e34c
rayudubobbili <rayudubobbili@gmail.com>
2018-09-07 21:10:17 +0530

    changing urls files to make all urls work

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from .views import homepage, Post_list_view, Post_detail_view
 app_name = 'blog'
 urlpatterns = [
     path('', homepage, name="homeview"),
-    path('posts', Post_list_view.as_view(), name="listview"),
-    path('<slug:slug>', Post_detail_view.as_view(), name="detailview"),
+    path('posts/', Post_list_view.as_view(), name="listview"),
+    path('<slug:slug>/', Post_detail_view.as_view(), name="detailview"),
 ]
