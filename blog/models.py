c863d420e1928b8942c2387bf3772e8e54f34f7c
rayudubobbili <rayudubobbili@gmail.com>
2018-09-08 01:38:49 +0530

    removing category class from models.py file

--- blog/models.py
+++ blog/models.py
@@ -51,20 +51,12 @@ class Likes(models.Model):
         verbose_name_plural = "Likes"
 
 
-class Category(models.Model):
-    category = models.SlugField(max_length=200)
-
-    def __str__(self):
-        return self.category
-
-
 class Post(models.Model):
     author = models.ForeignKey('auth.user', on_delete='CASCADE')
     title = models.CharField(max_length=200)
     meta = models.CharField(max_length=600)
     slug = models.SlugField(max_length=200)
     tags = models.ManyToManyField('Tags')
-    category = models.ManyToManyField('Category')
     body = models.TextField()
     body_html = models.TextField(editable=False)
     publish = models.BooleanField(default=False)
